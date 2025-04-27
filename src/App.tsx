949c61e2891e70f723653bc5430e6aa4cbb49cd0
vistasedeproposito <gracielegslouback@gmail.com>
2025-04-27 08:14:06 -0300

    Adicionadas rotas para quiz-builder, editor-unificado, editor-resultado e settings

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import UTMAnalyticsPage from './pages/admin/UTMAnalyticsPage';
 import { Toaster } from './components/ui/toaster';
+import QuizBuilderPage from './pages/QuizBuilderPage';
+import UnifiedEditorPage from './pages/editor/UnifiedEditorPage';
+import EditorResultadoPage from './pages/admin/EditorResultadoPage';
+import SettingsPage from './pages/admin/SettingsPage';
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Route path="/" element={<AdminDashboard />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/utm-analytics" element={<UTMAnalyticsPage />} />
+        <Route path="/admin/quiz-builder" element={<QuizBuilderPage />} />
+        <Route path="/admin/editor-unificado" element={<UnifiedEditorPage />} />
+        <Route path="/admin/editor-resultado" element={<EditorResultadoPage />} />
+        <Route path="/admin/settings" element={<SettingsPage />} />
         {/* Other routes will be added here */}
       </Routes>
       <Toaster />
