ec0d388af5d44bc1a4aff2d757fbda9fe6f8f7b9
vistasedeproposito <gracielegslouback@gmail.com>
2025-04-27 08:05:23 -0300

    Unificação dos estilos globais no index.css, remoção do App.css e ajustes no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import UTMAnalyticsPage from './pages/admin/UTMAnalyticsPage';
