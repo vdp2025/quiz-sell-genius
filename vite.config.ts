bf6781c015b69183af8f0aa9a859b3b503d9b733
Seu Nome <gracielegslouback@gmail.com>
2025-04-29 13:32:58 -0300

    chore: atualiza configuração do Vite para React e dependências do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
